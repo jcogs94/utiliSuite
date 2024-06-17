7be847f1f57cc92d89d09077ca3afd433c075faa
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-16 22:06:15 -0400

    back and front end init

---
+++ api/src/test/java/com/utilisuite/utilisuite/UtilisuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utilisuite.utilisuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilisuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
