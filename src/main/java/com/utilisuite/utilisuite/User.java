7f7a7d3b14075c0d84a2c026837f43cfb8b7ea6d
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-17 15:16:06 -0400

    model adjusted and all routes created

--- src/main/java/com/utilisuite/utilisuite/User.java
+++ src/main/java/com/utilisuite/utilisuite/User.java
@@ -11,10 +11,18 @@ public class User {
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
-    private String name;
-
+    // User data
+    private String userName;
     private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
 
+    // Util preferences defined by user
+    private Integer tipCalculator;
+    private Integer percentageCalculator;
+
+    // ID getter and setter
     public Integer getId() {
         return id;
     }
@@ -23,19 +31,73 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+
+    // userName getter and setter
+    public String getUserName() {
+        return userName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
+
+    // email getter and setter
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+
+    // password getter and setter
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    // firstName getter and setter
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    
+    // lastName getter and setter
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    
+    // tipCalculator getter and setter
+    public Integer getTipCalculator() {
+        return tipCalculator;
+    }
+
+    public void setTipCalculator(Integer tipCalculator) {
+        this.tipCalculator = tipCalculator;
+    }
+    
+    
+    // percentageCalculator getter and setter
+    public Integer getPercentageCalculator() {
+        return percentageCalculator;
+    }
+
+    public void setPercentageCalculator(Integer percentageCalculator) {
+        this.percentageCalculator = percentageCalculator;
     }
 }
