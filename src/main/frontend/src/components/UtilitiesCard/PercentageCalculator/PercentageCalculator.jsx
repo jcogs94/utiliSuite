f1696c5a0679cffedf18ca03779862a5a4595d54
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-18 11:05:37 -0400

    % calc init

---
+++ src/main/frontend/src/components/UtilitiesCard/PercentageCalculator/PercentageCalculator.jsx
@@ -0,0 +1,11 @@
+import './PercentageCalculator.css'
+
+const PercentageCalculator = () => {
+    return <>
+        <div id="percentage-calculator">
+            <h1>TEST</h1>
+        </div>
+    </>
+}
+
+export default PercentageCalculator
