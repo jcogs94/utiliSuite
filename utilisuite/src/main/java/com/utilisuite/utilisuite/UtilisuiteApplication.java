d38832bb7fbf402027571b3b14fa0c788d62b5b6
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-18 09:49:58 -0400

    file clean-up

---
+++ utilisuite/src/main/java/com/utilisuite/utilisuite/UtilisuiteApplication.java
@@ -0,0 +1,13 @@
+package com.utilisuite.utilisuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilisuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilisuiteApplication.class, args);
+	}
+
+}
