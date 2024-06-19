33d6fd48d7d85c9bdb9297238df6f8e5bfb78b1c
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-19 10:26:02 -0400

    CORS added, pom updates for combining front and back

--- src/main/java/com/utilisuite/utilisuite/MainController.java
+++ src/main/java/com/utilisuite/utilisuite/MainController.java
@@ -4,17 +4,18 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller // This means that this class is a Controller
+@CrossOrigin(origins = "http://localhost:3000") // Adds CORS to entire controller
 @RequestMapping(path="/api") // This means URL's start with /demo (after Application path)
 public class MainController {
     @Autowired // This means to get the bean called userRepository
